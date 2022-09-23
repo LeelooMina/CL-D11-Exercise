2df03257f32b32f4658dfd5a0246950bcaea0281
Crystal Campbell <blankpage@gmail.com>
2022-09-23 18:22:01 -0500

    ADD: Exercise 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
+    NavbarComponent,
 
   ],
   imports: [
